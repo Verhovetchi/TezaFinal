2ddc4835569b51304580101f7297cd18fb082c58
Verhovetchi Daniel <daniel.verhovetchi@ati.utm.md>
2021-02-09 14:02:12 +0200

    Second commit

--- 1/Final - Copy/Final/Controllers/HomeController.cs
+++ 1/Final - Copy/Final/Controllers/HomeController.cs
@@ -50,7 +50,7 @@ namespace Final.Controllers
           {
                string from = Request["from"];
                string subject = Request["subject"];
-               string body = Request["body"].ToString();
+               string body = Request["body"];
                
 
                using (MailMessage mail = new MailMessage())
@@ -84,19 +84,169 @@ namespace Final.Controllers
 
           public ActionResult Team(int id)
           {
-               var players = soccerDB.Database.SqlQuery<Player>("SELECT * FROM Players where TeamId=" + id);
+               var players = soccerDB.Database.SqlQuery<Player>("SELECT * FROM Players where TeamId=" + id).OrderBy(i => i.Number);
 
                ViewBag.Players = players;
 
                return View();
           }
 
+          public ActionResult Statistics(int id)
+          {
+               string path= "a";
+               switch (id)
+               {
+                    case 1:
+                         {
+                              path = "https://www.fctables.com/uploads/infographics/profil/276267/marcandre_ter_stegen.jpg";
+                              break;
+                         }
+                    case 2:
+                         {
+                              path = "https://www.fctables.com/uploads/infographics/profil/358372/sergino-dest.jpg";
+                              break;
+                         }
+                    case 3:
+                         {
+                              path = "https://www.fctables.com/uploads/infographics/profil/241949/gerard_pique.jpg";
+                              break;
+                         }
+                    case 4:
+                         {
+                              path = "https://www.fctables.com/uploads/infographics/profil/364283/ronald-araujo.jpg";
+                              break;
+                         }
+                    case 5:
+                         {
+                              path = "https://www.fctables.com/uploads/infographics/profil/312389/sergio_busquets.jpg";
+                              break;
+                         }
+                    case 6:
+                         {
+                              path = "https://www.fctables.com/uploads/infographics/profil/287113/miralem_pjanic.jpg";
+                              break;
+                         }
+                    case 7:
+                         {
+                              path = "https://www.fctables.com/uploads/infographics/profil/208981/antoine_griezmann.jpg";
+                              break;
+                         }
+                    case 8:
+                         {
+                              path = "https://www.fctables.com/uploads/infographics/profil/287113/miralem_pjanic.jpg";
+                              break;
+                         }
+                    case 9:
+                         {
+                              path = "https://www.fctables.com/uploads/infographics/profil/279811/martin_braithwaite.jpg";
+                              break;
+                         }
+                    case 10:
+                         {
+                              path = "https://www.fctables.com/uploads/infographics/profil/271592/lionel_messi.jpg";
+                              break;
+                         }
+                    case 11:
+                         {
+                              path = "https://www.fctables.com/uploads/infographics/profil/296003/ousmane_dembele.jpg";
+                              break;
+                         }
+               }
 
+               ViewBag.Path = path;
+               return View();
+          }
 
+          public ActionResult Iniesta()
+          {
+               return View();
+          }
 
-          //public ActionResult Test()
+          public ActionResult Iniesta2()
+          {
+               return View();
+          }
+
+          [HttpGet]
+          public ActionResult AllPlayers()
+          {
+               var players = soccerDB.Players.Include(t => t.Team);
+               return View(players.ToList());
+          }
+
+          [HttpGet]
+          public ActionResult CreatePlayer()
+          {
+               SelectList teams = new SelectList(soccerDB.Teams, "Id", "Name");
+               ViewBag.Teams = teams;
+
+               return View();
+          }
+
+          [HttpPost]
+          public ActionResult CreatePlayer(Player player)
+          {
+               soccerDB.Players.Add(player);
+               soccerDB.SaveChanges();
+               return RedirectToAction("Index");
+          }
+
+          [HttpGet]
+          public ActionResult DeletePlayer(int id)
+          {
+               Player p = soccerDB.Players.Find(id);
+
+               if (p == null)
+                    return HttpNotFound();
+
+               return View(p);
+          }
+
+          [HttpPost, ActionName("DeletePlayer")]
+          public ActionResult Delete(int id)
+          {
+               Player p = soccerDB.Players.Find(id);
+
+               if (p == null)
+                    return HttpNotFound();
+
+               soccerDB.Players.Remove(p);
+               soccerDB.SaveChanges();
+
+               return RedirectToAction("Index");
+          }
+
+          [HttpGet]
+          public ActionResult EditPlayer(int? id)
+          {
+               if (id == null)
+                    return HttpNotFound();
+
+               Player player = soccerDB.Players.Find(id);
+
+               SelectList teams = new SelectList(soccerDB.Teams, "Id", "Name");
+               ViewBag.Teams = teams;
+
+               if (player != null)
+                    return View(player);
+
+               return HttpNotFound();
+          }
+
+          [HttpPost, ActionName("EditPlayer")]
+          public ActionResult Edit(Player player)
+          {
+               soccerDB.Entry(player).State = EntityState.Modified;
+               soccerDB.SaveChanges();
+               return RedirectToAction("Index");
+          }
+
+          //[HttpPost]
+          //public ActionResult CreatePlayer(Player player)
           //{
-          //     SoccerContext soccer = new SoccerContext();
+          //     soccerDB.Players.Add(player);
+          //     soccerDB.SaveChanges();
+          //     return View();
           //}
 
           //SoccerContext db2 = new SoccerContext();
